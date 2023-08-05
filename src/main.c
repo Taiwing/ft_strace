3276a21d9f347a0be78db96ce07446d701f9b668
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-08-05 13:33:28 +0200

    feat: handle signals as per the subject

--- src/main.c
+++ src/main.c
@@ -8,6 +8,23 @@ static void	config_cleanup(void)
 		free(g_cfg.pid_table);
 }
 
+void	set_signals(void)
+{
+	sigset_t	empty;
+	sigset_t	blocked;
+
+	if (sigemptyset(&empty))
+		err(EXIT_FAILURE, "sigemptyset");
+	if (sigprocmask(SIG_SETMASK, &empty, NULL))
+		err(EXIT_FAILURE, "sigprocmask");
+	if (sigemptyset(&blocked))
+		err(EXIT_FAILURE, "sigemptyset");
+	if (sigaddset(&blocked, SIGHUP) || sigaddset(&blocked, SIGINT)
+		|| sigaddset(&blocked, SIGQUIT) || sigaddset(&blocked, SIGPIPE)
+		|| sigaddset(&blocked, SIGTERM))
+		err(EXIT_FAILURE, "sigaddset");
+}
+
 int	main(int argc, char **argv)
 {
 	char		**args = NULL;
@@ -19,6 +36,7 @@ int	main(int argc, char **argv)
 	if (!*args && !g_cfg.pid_table)
 		error(EXIT_FAILURE, 0, "must have 'command [args]' or '-p pid'\n"
 			"Try '%s -h' for more information.", program_invocation_name);
+	set_signals();
 
 	if (g_cfg.pid_table)
 	{
