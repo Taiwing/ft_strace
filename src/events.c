788050136d601dac31ea01a983b379dec5ef997b
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-08-19 20:33:08 +0200

    fix: do not print "unfinished" if interrupted
    unfinished has already been printed

--- src/events.c
+++ src/events.c
@@ -73,7 +73,8 @@ void		process_event_loop(t_st_config *cfg)
 			if (cfg->current_process == cfg->child_process)
 				cfg->exit_code = WEXITSTATUS(status);
 		} else if (WIFSIGNALED(status)) {
-			if (cfg->current_process->in_syscall)
+			if (cfg->current_process->in_syscall
+				&& !cfg->current_process->interrupted_syscall)
 				stprintf(NULL, " <unfinished ...>\n");
 			stprintf(cfg, "+++ killed by %s %s+++\n", signame(WTERMSIG(status)),
 				WCOREDUMP(status) ? "(core dumped) " : "");
